12d55a52b85394a7b10b053c96820bfb0f2ee3cd
Jean-Luc19 <aticus19@gmail.com>
2017-01-31 12:28:54 -0700

    map and foreach function added to TMNT.js

--- tmnt.js
+++ tmnt.js
@@ -1,8 +1,22 @@
-var arr = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
+ var arr = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
 
-
+//Filtered example
 var filtered = arr.filter(function(item){
   return item[0] >= 0 && item[1] >= 0;
 });
 
 console.log(filtered);
+
+//Map example
+var totalSteps = arr.map(function(item){
+  return Math.abs(item[0]) + Math.abs(item[1]);
+});
+console.log(totalSteps);
+
+//forEach example
+totalSteps.forEach(function(item){
+  console.log(item);
+});
+
+//ES6 forEach Version...
+//totalSteps.forEach(item => console.log(item));
