c6ff7b5701f489a189df2da6877c1ff7d6d4e22e
Jean-Luc19 <aticus19@gmail.com>
2017-01-31 13:08:32 -0700

    refactored filtered method

--- tmnt.js
+++ tmnt.js
@@ -1,21 +1,31 @@
- var arr = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]];
+ var arr = [[0, 0], [0, 5, 4, 2], [-1, -3], [-3, 1], [2, -4], [3, 2]];
 
 //Filtered example
-var filtered = arr.filter(function(item){
-  return item[0] >= 0 && item[1] >= 0;
-});
+// var filtered = arr.filter(function(item){
+//   return item[0] >= 0 && item[1] >= 0;
+// });
 
+//Filtering the array without hardcoding in the indices
+
+var filtered = arr.filter(function(item){
+  return item.every(function(x){
+    return x >= 0;
+  })
+
+  });
+
+//we want to filter the two dimensional array, if any of the items are negative in the inner arrays that one wont pass the filter
 console.log(filtered);
 
 //Map example
 var totalSteps = arr.map(function(item){
   return Math.abs(item[0]) + Math.abs(item[1]);
 });
-console.log(totalSteps);
+//console.log(totalSteps);
 
 //forEach example
 totalSteps.forEach(function(item){
-  console.log(item);
+  //console.log(item);
 });
 
 //ES6 forEach Version...
